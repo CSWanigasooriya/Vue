c5f684ab05dcf53756031456cbbaef132a453bfa
CSWanigasooriya <chamathwanigasooriya@gmail.com>
2021-10-12 11:52:00 +0530

    backend updates

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,17 +2,32 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-
+const { sequelize } = require('./models')
+const config = require('./config/config')
 
 const app = express();
-app.use(morgan('combined'))
-app.use(bodyParser.json())
-app.use(cors())
 
-app.post('/register', (req, res) => {
-    res.send({
-        message: `Hello ${req.body.email}! Your user was registered`
-    })
+var corsOptions = {
+    origin: "http://localhost:8080"
+};
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan('combined'))
+
+app.use(bodyParser.json())
+
+require('./routes')(app)
+
+
+sequelize.sync().then(() => {
+    app.listen(config.port)
+    console.log(`Server started on port ${config.port}`)
 })
 
-app.listen(process.env.PORT || 8081)
---
+++ backend/src/config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    port: process.env.PORT || 8081,
+    db: {
+        database: process.env.DB_NAME || 'studentmanagement',
+        user: process.env.DB_USER || 'root',
+        password: process.env.DB_PASS || '',
+        options: {
+            dialect: process.env.DIALECT || 'mysql',
+            host: process.env.HOST || 'localhost',
+            pool: {
+                max: 5,
+                min: 0,
+                acquire: 30000,
+                idle: 10000
+            }
+        }
+    }
+}
